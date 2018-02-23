ce6a584fbf9dc16ac707f984d5ffb848e8af89c7
Micah J. Martin <mjm5097@rit.edu>
2018-02-23 02:03:56 -0500

    Changed decryption template

--- Server/server.py
+++ Server/server.py
@@ -2,9 +2,16 @@ from flask import render_template
 from app import app
 
 
-@app.route("shy/")
-def handout_shy():
+@app.route("/shy")
+def shy_deply():
     data = {}
     data['password'] = "unlockme"
     data['server'] = "10.0.0.1"
-    return render_template("shy.j2", data=data)
+    return render_template("shy_script.j2", data=data)
+
+@app.route("/shy/decrypt")
+def shy_decrypt():
+    data = {}
+    data['password'] = "unlockme"
+    data['files'] = ["file.txt", "/home/"]
+    return render_template("shy_decryptor.j2", data=data)
